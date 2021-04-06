475865398a49385b6708f35fba242014e64eb961
Welder n. Fernandes <Welderx3@gmail.com>
2021-04-06 18:13:38 -0300

    feat: add service module

---
+++ app/Models/Service.ts
@@ -0,0 +1,28 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Service extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public value: number
+
+  @column()
+  public category_id: number
+
+  @column()
+  public timer_id: number
+
+  @column()
+  public description: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
